ac78e70b306a60b17a6a53f066defca3f3c1b49f
Raforawesome <ubuntuhasnoroblox@gmail.com>
2025-10-01 17:32:47 -0400

    Add leptos & leptos_axum hello world

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,11 @@ async fn main() {
     let auth_store = MemoryAuthStore::new();
     debug!("Authentication store initialized");
 
+    // Set up leptos webui
+    let leptos_conf = get_configuration(None).unwrap();
+    let leptos_options = leptos_conf.leptos_options;
+    let routes = generate_route_list(foundry::webui::App);
+
     // Create the router with all API routes
     let app = axum::Router::new()
         .route("/", get(async || "Welcome to Foundry BaaS!"))
@@ -48,8 +53,14 @@ async fn main() {
             post(api::auth::register::<ConcreteAuthStore>),
         )
         .route("/api/v1/login", post(api::auth::login::<ConcreteAuthStore>))
-        .layer(RequestDecompressionLayer::new()) // decompress incoming requests
-        .layer(CompressionLayer::new()) // compress responses (auto negotaties)
+        .fallback(async || StatusCode::NOT_FOUND)
+        .layer(
+            RequestDecompressionLayer::new()
+                .br(true)
+                .gzip(true)
+                .pass_through_unaccepted(false),
+        ) // decompress incoming requests
+        .layer(CompressionLayer::new()) // compress responses (auto negotiates)
         .layer(ClientIpSource::ConnectInfo.into_extension()) // provide client ip extractors
         .with_state(Arc::new(auth_store)); // put auth store in global state
 
@@ -63,4 +74,11 @@ async fn main() {
     )
     .await
     .unwrap();
+}
+
+#[cfg(not(feature = "ssr"))]
+fn main() {
+    // no client-side main function
+    // unless we want this to work with e.g., Trunk for pure client-side testing
+    // see lib.rs for hydration function instead
 }
---
+++ src/webui.rs
@@ -0,0 +1,55 @@
+use leptos::prelude::*;
+use leptos_meta::{MetaTags, Stylesheet, Title, provide_meta_context};
+use leptos_router::{
+    StaticSegment,
+    components::{Route, Router, Routes},
+};
+
+pub fn shell(options: LeptosOptions) -> impl IntoView {
+    view! {
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <AutoReload options=options.clone() />
+                <HydrationScripts options/>
+                <MetaTags/>
+            </head>
+            <body>
+                <App/>
+            </body>
+        </html>
+    }
+}
+
+#[component]
+pub fn App() -> impl IntoView {
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+    provide_meta_context();
+
+    view! {
+        // injects a stylesheet into the document <head>
+        // id=leptos means cargo-leptos will hot-reload this stylesheet
+        <Stylesheet id="leptos" href="/pkg/axtest.css"/>
+
+        // sets the document title
+        <Title text="Welcome to Leptos"/>
+
+        // content for this welcome page
+        <Router>
+            <main>
+                <Routes fallback=|| "Page not found.".into_view()>
+                    <Route path=StaticSegment("") view=Home/>
+                </Routes>
+            </main>
+        </Router>
+    }
+}
+
+#[component]
+pub fn Home() -> impl IntoView {
+    view! {
+        <h1>Hello, world</h1>
+    }
+}
