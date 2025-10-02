2fc438a79f7b09f3737d8c889a2fb53f2a28e050
Raforawesome <ubuntuhasnoroblox@gmail.com>
2025-10-02 12:59:54 -0400

    enable hydration and gate api/storage behind server

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
+#[cfg(feature = "ssr")]
 pub mod api;
+#[cfg(feature = "ssr")]
 pub mod storage;
+
 pub mod webui;
 
 #[cfg(feature = "hydrate")]
 #[wasm_bindgen::prelude::wasm_bindgen]
 pub fn hydrate() {
     console_error_panic_hook::set_once();
-    // leptos::mount::hydrate_body(App);
+    leptos::mount::hydrate_body(webui::App);
 }
