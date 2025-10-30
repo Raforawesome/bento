4b23aebe92cc3d8bf7b7cc8ae09153eacb0c0ee6
Raforawesome <ubuntuhasnoroblox@gmail.com>
2025-10-30 14:38:07 -0400

    Add a secrets manager to `config.rs`

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,7 @@
 use std::sync::LazyLock;
 
-use serde::Deserialize;
+use axum_extra::extract::cookie;
+use serde::{Deserialize, Serialize};
 use toml::de;
 
 pub static LOCAL_CONF: LazyLock<Config> =
@@ -26,4 +27,67 @@ pub struct Admin {
 pub fn grab_config() -> Result<Config, de::Error> {
     let config_str = std::fs::read_to_string("bento.toml").expect("Failed to read bento.toml");
     toml::from_str(&config_str)
+}
+
+/*
+ * Secrets Manager
+ */
+use std::fs;
+use std::path::Path;
+
+pub struct CookieKey(cookie::Key);
+
+impl CookieKey {
+    pub fn generate() -> Self {
+        CookieKey(cookie::Key::generate())
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Secrets {
+    pub cookie_key: CookieKey,
+}
+
+impl Secrets {
+    pub fn load() -> Result<Self, Box<dyn std::error::Error>> {
+        let secrets_path = ".bento_secrets";
+        if Path::new(secrets_path).exists() {
+            let secrets_str = fs::read_to_string(secrets_path)?;
+            let secrets: Secrets = toml::from_str(&secrets_str)?;
+            Ok(secrets)
+        } else {
+            // Generate a new cookie key if secrets file doesn't exist
+            let secrets = Secrets {
+                cookie_key: CookieKey::generate(),
+            };
+            secrets.save()?;
+            Ok(secrets)
+        }
+    }
+
+    pub fn save(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let secrets_toml = toml::to_string(self)?;
+        fs::write(".bento_secrets", secrets_toml)?;
+        Ok(())
+    }
+}
+
+// serde glue for saving cookie key
+impl Serialize for CookieKey {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_bytes(self.0.master())
+    }
+}
+
+impl<'de> Deserialize<'de> for CookieKey {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let bytes = <Vec<u8>>::deserialize(deserializer)?;
+        Ok(CookieKey(cookie::Key::from(&bytes)))
+    }
 }
