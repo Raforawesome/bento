a6ee0e0d0dc99dc1eca7e27a71cfa614aa3ba7e5
Raforawesome <ubuntuhasnoroblox@gmail.com>
2025-12-01 22:53:09 -0500

    refactor: abstract `UsersIcon` to `icons.rs`

--- src/webui/icons.rs
+++ src/webui/icons.rs
@@ -82,3 +82,12 @@ pub fn LogoutIcon(#[prop(optional)] class: &'static str) -> impl IntoView {
         </svg>
     }
 }
+
+#[component]
+pub fn UsersIcon(#[prop(optional)] class: &'static str) -> impl IntoView {
+    view! {
+        <svg class=class xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.354a4 4 0 1 1 0 5.292M15 21H3v-1a6 6 0 0 1 12 0v1zm0 0h6v-1a6 6 0 0 0-9-5.197M13 7a4 4 0 1 1-8 0 4 4 0 0 1 8 0z" />
+        </svg>
+    }
+}
--- src/webui/screen_home.rs
+++ src/webui/screen_home.rs
@@ -1,4 +1,3 @@
-use crate::types::AppError;
 use crate::webui::icons::*;
 use crate::webui::{CurrentUser, LogoSvg, Logout};
 use leptos::form::ActionForm;
@@ -154,9 +153,7 @@ fn NavBar(user: CurrentUser) -> impl IntoView {
                                 class="flex items-center w-full px-4 py-3 text-sm text-gray-300 hover:bg-[#252630] hover:text-white transition"
                                 on:click=move |_| set_dropdown_open.set(false)
                             >
-                                <svg class="w-4 h-4 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
-                                </svg>
+                                <UserIcon class="w-4 h-4 mr-3" />
                                 "Manage Users"
                             </button>
 
